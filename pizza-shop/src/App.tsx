3bcfabdb1960bd6229192b0ef9a4dbac4c563e47
gabriel <gabrielcomputacao@outlook.com>
2024-02-26 06:29:30 -0300

    feat: creating layout auth and  page sign-in

--- pizza-shop/src/App.tsx
+++ pizza-shop/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import "./global.css";
 
 export function App() {
   return (
--- pizza-shop/src/pages/_layouts/auth.tsx
+++ pizza-shop/src/pages/_layouts/auth.tsx
@@ -1,11 +1,21 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="grid min-h-screen  grid-cols-2">
+      {/*barra 5 (/5) quer dizer apenas 5% de opacidade */}
+      <div className="flex h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground  flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span>Pizza Shop</span>
+        </div>
+        <footer>
+          Painel do Parceiro &copy Pizza Shop - {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col justify-center items-center">
         {/* Outlet mostra onde o componente de fora vai ficar dentro do layout */}
         <Outlet />
       </div>
