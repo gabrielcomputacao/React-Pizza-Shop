d826aadfe848e70b4588704480c77824b28babde
gabriel <gabrielcomputacao@outlook.com>
2024-02-26 05:40:43 -0300

    feat: configuring react helmet async

--- pizza-shop/src/App.tsx
+++ pizza-shop/src/App.tsx
@@ -1,6 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <HelmetProvider>
+      {/* com esse titleTemplate colocando %s vai receeber um variavel que vai vir dentro das paginas e depois colocar o resto do titulo no template title */}
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router}></RouterProvider>
+    </HelmetProvider>
+  );
 }
