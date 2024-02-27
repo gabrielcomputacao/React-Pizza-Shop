0018015d3f054e7bb7e11f3eedd22774776feed2
gabriel <gabrielcomputacao@outlook.com>
2024-02-27 05:08:26 -0300

    feat: creating and configuring react hook form

--- pizza-shop/src/pages/auth/sign-in.tsx
+++ pizza-shop/src/pages/auth/sign-in.tsx
@@ -2,8 +2,25 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+// todo => o infer transforma a tipagem do zod em tipagem que se enquadra no typescript
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  function handleSignIn(data: SignInForm) {}
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,13 +35,21 @@ export function SignIn() {
             </p>
           </div>
 
-          <form action="" className="space-y-4">
+          {/* uma função recendo a outra é até um pattern que se chama HOF, uma função de maior valor recebe outra,
+            handle submit chama a função que foi criada
+          
+          */}
+          <form
+            action=""
+            className="space-y-4"
+            onSubmit={handleSubmit(handleSignIn)}
+          >
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar Painel
             </Button>
           </form>
