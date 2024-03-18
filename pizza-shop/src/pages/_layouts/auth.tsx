ec41c71a2543eb5c55a3f4e97fec51a6863990ef
gabriel <gabrielcomputacao@outlook.com>
2024-03-18 07:40:43 -0300

    feat: creating the application header

--- pizza-shop/src/pages/_layouts/auth.tsx
+++ pizza-shop/src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen  grid-cols-2">
+    <div className="grid min-h-screen  grid-cols-2 antialiased">
       {/*barra 5 (/5) quer dizer apenas 5% de opacidade */}
       <div className="flex h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground  flex-col justify-between">
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
