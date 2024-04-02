584ecb3b34c36b0d3920e6d9683ab7ab33d959ad
gabriel <gabrielcomputacao@outlook.com>
2024-04-02 06:01:01 -0300

    feat: creating table in orders page

--- pizza-shop/src/routes.tsx
+++ pizza-shop/src/routes.tsx
@@ -4,6 +4,7 @@ import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 /* 
     Cada objeto dentro do array é uma rota da aplicação
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Dashboard />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
       },
     ],
   },
